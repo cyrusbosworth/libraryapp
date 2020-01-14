2a37216041f04bb34ca315b75cfa260487b9aad7
Cyrus Kurtz <cyrus.bosworth@gmail.com>
2020-01-14 12:10:10 -0500

    book create and file upload

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -27,6 +28,7 @@ db.on('error', error => console.error(error));
 db.once('open', error => console.log('Connected to Mongo'));
 
 app.use('/', indexRouter);
-app.use('/authors/', authorRouter);
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 3000);
